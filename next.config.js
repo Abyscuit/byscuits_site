e890e691fe5cfb2fc30cd260489851a74770666c
Abyscuit <marcus14530@gmail.com>
2024-01-23 05:51:24 -0800

    devop: update packages and setup env variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  env: {
+    DISCORD_CLIENTID: process.env.DISCORD_CLIENTID,
+    DISCORD_SECRET: process.env.DISCORD_SECRET,
+    DISCORD_OAUTH: process.env.DISCORD_OAUTH,
+  },
 };
 
 module.exports = nextConfig;
