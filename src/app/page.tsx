6ee5d2d75a6e9953fb31e2eaf7ec4d6a7dc1d40e
Abyscuit <36307949+Abyscuit@users.noreply.github.com>
2024-08-16 22:13:29 -0700

    chore: add text to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,29 @@
 export default function Home() {
+  const discordLink = 'https://discord.gg/';
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div>
-        <h1 className='scroll-m-20 border-b pb-2 text-4xl font-extrabold tracking-tight lg:text-5xl'>Heading 1</h1>
-        <small className='text-sm font-medium leading-none'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores sed,
-          temporibus porro, excepturi suscipit, repudiandae voluptatem quas
-          exercitationem quia fugiat minus molestias facere cupiditate deleniti
-          ad omnis eum beatae fuga alias ipsam a recusandae? Possimus sint est
-          sunt sit ab nesciunt deleniti cum id veniam. Cum eum rem inventore.
-          Neque.
+        <h1 className='scroll-m-20 border-b pb-2 text-4xl font-extrabold tracking-tight lg:text-5xl'>
+          Da Byscuits are back!
+        </h1>
+        <small className='text-sm leading-none text-muted-foreground'>
+          The original server was banned and so was my account since it was the
+          owner. Discord has refused to elaborate on the reason but we are back
+          with a new server!
+          <br /> <br />
+          Join the new server here:{' '}
+          <a
+            href={discordLink}
+            target='_blank'
+            rel='noopener noreferrer'>
+            {discordLink}
+          </a>
         </small>
       </div>
       <div>
-        <h2 className='scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0'>Heading 2</h2>
+        <h2 className='scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0'>
+          Heading 2
+        </h2>
         <p className='text-sm text-muted-foreground'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
           suscipit eligendi ex magnam a libero numquam labore quae impedit
@@ -21,7 +31,9 @@ export default function Home() {
         </p>
       </div>
       <div>
-        <h3 className='scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight'>Heading 3</h3>
+        <h3 className='scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight'>
+          Heading 3
+        </h3>
         <p className='text-sm text-muted-foreground'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi fugit
           officiis fuga amet ipsa? Explicabo adipisci quaerat, nulla quibusdam
